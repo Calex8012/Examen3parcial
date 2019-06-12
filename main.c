a2757e3ad606c7cb6ca78233f18b8aa1f2687ffb
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2019-06-12 13:00:15 -0500

    prueba

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <18F4620.h>
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
-#use delay(clock=16000000)
+#use delay(clock=40000000)
 
 #define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
 
@@ -13,10 +13,7 @@
 
 void main (void){
    setup_oscillator(OSC_16MHZ);
-#ifdef __DEBUG_SERIAL__ //Deberiamos de proteger nuestras depuraciones de esta forma o usar una macro ya protegida.
-   printf("Hola Mundo\n");//Puedes usar putc o printf. Revisa la documentación de CCS para ver que mas puedes hacer.
-#endif
    while(1){
-      
+      set_tris_a(0x02);
    }
 }
